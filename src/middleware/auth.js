cc094b97a51aebb612416bd3cb736a1d98edf017
omar osama <omarosama200100@gmail.com>
2019-05-09 17:12:07 +0200

    logout feature

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,6 +13,7 @@ const auth = async (req, res, next) => {
 		}
 
 		req.user = user;
+		req.token = token;
 		next();
 	} catch (e) {
 		res.status(401).send({ error: 'please authenticate' });
--- src/routes/user.js
+++ src/routes/user.js
@@ -26,6 +26,18 @@ router.post('/users/login', async (req, res) => {
 	}
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+
+		await req.user.save();
+
+		res.send();
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
 router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user);
 });
