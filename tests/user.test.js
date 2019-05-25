383e0a86218ba6ab3d2595854ccf6b7534702df9
omar osama <omarosama200100@gmail.com>
2019-05-25 15:32:34 +0200

    manage tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,8 +1,27 @@
 const request = require('supertest');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('../src/models/user');
 const app = require('../src/app');
 
-beforeEach(() => {
-	console.log('before each');
+const userOneId = new mongoose.Types.ObjectId();
+const userOneToken = jwt.sign({ _id: userOneId }, process.env.JWT_SECRET);
+
+const userOne = {
+	_id      : userOneId,
+	email    : 'user@user.user',
+	password : 'user123!!',
+	name     : 'user one',
+	tokens   : [
+		{
+			token : userOneToken
+		}
+	]
+};
+
+beforeEach(async () => {
+	await User.deleteMany();
+	await new User(userOne).save();
 });
 
 afterEach(() => {
@@ -10,12 +29,52 @@ afterEach(() => {
 });
 
 test('should user signup', async () => {
-	await request(app)
+	const responce = await request(app)
 		.post('/users')
 		.send({
-			name: 'omar',
-			email: 'omar@gmaill.com',
-			password: 'helloworld!'
+			name     : 'omar',
+			email    : 'omar@gmaill.com',
+			password : 'helloworld!'
 		})
 		.expect(201);
+
+	const user = await User.findById(responce.body.user._id);
+
+	expect(user).not.toBeNull();
+
+	expect(responce.body).toMatchObject({
+		user  : {
+			name  : user.name,
+			email : user.email
+		},
+		token : user.tokens[0].token
+	});
+});
+
+test('should login user', async () => {
+	const responce = await request(app)
+		.post('/users/login')
+		.send({
+			email    : userOne.email,
+			password : userOne.password
+		})
+		.expect(200);
+});
+
+test('should not login for not valid data', async () => {
+	await request(app)
+		.post('/users/login')
+		.send({
+			email    : 'sasd',
+			password : 'sadgggg'
+		})
+		.expect(400);
+});
+
+test('should get profile for user', async () => {
+	await request(app).get('/users/me').set('Authorization', `Bearer ${userOneToken}`).send().expect(200);
+});
+
+test('should not login for unAuth user', async () => {
+	await request(app).get('/users/me').send().expect(401);
 });
